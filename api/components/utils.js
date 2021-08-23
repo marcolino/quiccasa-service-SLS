a2273cddb56da8463107aaa966c047d38e78a7b0
Marco Solari <marcosolari@gmail.com>
2021-08-23 20:18:41 +0200

    Added webpack and domain-manager plugins to serverless

--- api/components/utils.js
+++ api/components/utils.js
@@ -1,5 +1,26 @@
 'use strict';
 
+module.exports.httpJSONResponse = (statusCode = 200, message = "", input = null, headers = {}) => {
+  const jsonSpaces = 0;
+
+  return {
+    statusCode,
+    headers: {
+      //"Access-Control-Allow-Origin": "*", // TODO: do we really need this ?
+      "Content-Type": "application/json",
+      ...headers
+    },
+    body: JSON.stringify(
+      {
+        message,
+        input,
+      },
+      null, // replacer
+      jsonSpaces, // spaces
+    ),
+  };
+}
+
 module.exports.formatMoney = (number, locale, currency) => {
   return number.toLocaleString(locale, { style: 'currency', currency });
 }
---
+++ api/handler.js
@@ -0,0 +1,17 @@
+'use strict';
+
+//const config = require("../config");
+const { httpJSONResponse } = require("./components/utils");
+
+module.exports.unsubscribe = async (event) => {
+  const serviceName = "quiccasa"; // TODO: get from global environmnt/config...
+
+  console.log('event:', event);
+  const username = event.queryStringParameters.username;
+  if (!username) {
+    return httpJSONResponse(400, `Username should be specified`, event)
+  }
+
+  return httpJSONResponse(200, `You did successfully unsubscribe from ${serviceName}!`, event);
+};
+
